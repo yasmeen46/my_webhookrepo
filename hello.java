79c466120dd78c16f6795a0fcab98e42c688144f
yasmeen46 <yasmeenbanu4646@gmail.com>
2021-07-04 18:10:27 +0000

    add intial changes

---
+++ hello.java
@@ -0,0 +1,26 @@
+//New Changes are added
+//iiiiiiii
+package org.jacoco.examples.maven.java;
+
+public class HelloWorld {
+		
+		public String getMessage(boolean bigger) {
+					if (bigger) {
+									return "Hello Universe!";
+											} else {
+															return "Hello World, Hello Hello,Hello!";											}
+						}
+
+}package org.jacoco.examples.maven.java;
+
+public class HelloWorld {
+		
+		public String getMessage(boolean bigger) {
+					if (bigger) {
+									return "Hello Universe!";
+											} else {
+															return "Hello World!";
+																	}
+						}
+
+}
